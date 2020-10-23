fe66abcc0d073d62adb4679ee8700430a952d148
Ashna Nizam <68196153+AshNiz24@users.noreply.github.com>
2020-10-23 20:28:38 +0530

    Add files via upload

---
+++ Mobile Application/lib/components/text_input.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TextTimeInput extends StatelessWidget {
+  final String text;
+  TextTimeInput(this.text);
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      autocorrect: true,
+      decoration: InputDecoration(
+        suffixIcon: Icon(
+          Icons.access_time,
+          color: Color(0xff4A306D),
+          size: 25,
+        ),
+        hintText: text,
+        hintStyle: TextStyle(color: Colors.grey, fontSize: 18),
+        filled: true,
+        fillColor: Colors.white70,
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(12.0)),
+          borderSide: BorderSide(color: Color(0xff4A306D), width: 2),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          borderSide: BorderSide(color: Color(0xff4A306D)),
+        ),
+      ),
+    );
+  }
+}
