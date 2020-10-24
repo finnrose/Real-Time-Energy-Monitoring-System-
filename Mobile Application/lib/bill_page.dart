a04925d10ef6c76b0398dc0afe615f0390da3d12
Ashna Nizam <68196153+AshNiz24@users.noreply.github.com>
2020-10-24 12:01:45 +0530

    Update bill_page.dart

--- Mobile Application/lib/bill_page.dart
+++ Mobile Application/lib/bill_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:smart_energy_meter/home_page.dart';
 import 'components/round_button.dart';
 import 'components/row.dart';
 
