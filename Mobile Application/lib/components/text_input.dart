a929b0a9017a8ddcd6c0578f4ddea6c88b72bc91
finnrose <62615308+finnrose@users.noreply.github.com>
2020-10-25 23:40:41 +0530

    Update text_input.dart

--- Mobile Application/lib/components/text_input.dart
+++ Mobile Application/lib/components/text_input.dart
@@ -1,30 +1,54 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'file:///F:/Project/energy_monitoring/lib/notifications/notification_dialog.dart';
 
-class TextTimeInput extends StatelessWidget {
+class TextTimeInput extends StatefulWidget {
   final String text;
   TextTimeInput(this.text);
+
+  @override
+  _TextTimeInputState createState() => _TextTimeInputState();
+}
+
+class _TextTimeInputState extends State<TextTimeInput> {
+  DateTime selectedDate = DateTime.now();
+
+  final DateFormat dateFormat = DateFormat('dd-MM-yyyy   HH:mm');
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      autocorrect: true,
-      decoration: InputDecoration(
-        suffixIcon: Icon(
-          Icons.access_time,
-          color: Color(0xff4A306D),
-          size: 25,
-        ),
-        hintText: text,
-        hintStyle: TextStyle(color: Colors.grey, fontSize: 18),
-        filled: true,
-        fillColor: Colors.white70,
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(12.0)),
-          borderSide: BorderSide(color: Color(0xff4A306D), width: 2),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(10.0)),
-          borderSide: BorderSide(color: Color(0xff4A306D)),
+    return GestureDetector(
+      onTap: () async {
+        showDateTimeDialog(context, initialDate: selectedDate,
+            onSelectedDate: (selectedDate) {
+          setState(() {
+            this.selectedDate = selectedDate;
+            print(this.selectedDate);
+          });
+        });
+      },
+      child: AbsorbPointer(
+        child: TextFormField(
+          autocorrect: true,
+          decoration: InputDecoration(
+            suffixIcon: Icon(
+              Icons.access_time,
+              color: Color(0xff4A306D),
+              size: 25,
+            ),
+            hintText: widget.text,
+            hintStyle: TextStyle(color: Colors.grey, fontSize: 18),
+            filled: true,
+            fillColor: Colors.white70,
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(12.0)),
+              borderSide: BorderSide(color: Color(0xff4A306D), width: 2),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              borderSide: BorderSide(color: Color(0xff4A306D)),
+            ),
+          ),
         ),
       ),
     );
