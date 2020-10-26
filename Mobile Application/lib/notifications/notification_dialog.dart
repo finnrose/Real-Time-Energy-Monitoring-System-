1cac4cfea771b3d718ce4cc9720f6c7b3dd8cfe4
Ashna Nizam <68196153+AshNiz24@users.noreply.github.com>
2020-10-26 12:56:08 +0530

    Update notification_dialog.dart

--- Mobile Application/lib/notifications/notification_dialog.dart
+++ Mobile Application/lib/notifications/notification_dialog.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:smart_energy_meter/components/text_input.dart';
 
 Future<TimeOfDay> _selectTime(BuildContext context,
     {@required DateTime initialDate}) {
-  final now = DateTime.now();
+  //final now = DateTime.now();
 
   return showTimePicker(
-    context: context,
-    initialTime: TimeOfDay(hour: initialDate.hour, minute: initialDate.minute),
-  );
+      context: context,
+      initialTime:
+          TimeOfDay(hour: initialDate.hour, minute: initialDate.minute),
+      builder: (BuildContext context, Widget child) {
+        return Theme(
+          data: ThemeData.light().copyWith(
+            primaryColor: Color(0xff36244F),
+            accentColor: Color(0xff36244F),
+            colorScheme: ColorScheme.light(primary: const Color(0xff36244F)),
+            buttonTheme: ButtonThemeData(textTheme: ButtonTextTheme.primary),
+          ),
+          child: child,
+        );
+      });
 }
 
 Future<DateTime> _selectDateTime(BuildContext context,
@@ -17,19 +29,29 @@ Future<DateTime> _selectDateTime(BuildContext context,
   final newestDate = initialDate.isAfter(now) ? initialDate : now;
 
   return showDatePicker(
-    context: context,
-    initialDate: newestDate.add(Duration(seconds: 1)),
-    firstDate: DateTime(1970),
-    lastDate:now,
-  );
+      context: context,
+      initialDate: newestDate.add(Duration(seconds: 1)),
+      firstDate: DateTime(1970),
+      lastDate: now,
+      builder: (BuildContext context, Widget child) {
+        return Theme(
+            data: ThemeData.light().copyWith(
+              primaryColor: Color(0xff36244F),
+              accentColor: Color(0xff36244F),
+              colorScheme: ColorScheme.light(primary: const Color(0xff36244F)),
+              buttonTheme: ButtonThemeData(textTheme: ButtonTextTheme.primary),
+            ),
+            child: child);
+      });
 }
 
-Dialog showDateTimeDialog(
-    BuildContext context, {
-      @required ValueChanged<DateTime> onSelectedDate,
-      @required DateTime initialDate,
-    }) {
+showDateTimeDialog(
+  BuildContext context, {
+  @required ValueChanged<DateTime> onSelectedDate,
+  @required DateTime initialDate,
+}) {
   final dialog = Dialog(
+    backgroundColor: Color(0xFFCEC2FF),
     child: DateTimeDialog(
         onSelectedDate: onSelectedDate, initialDate: initialDate),
   );
@@ -62,71 +84,94 @@ class _DateTimeDialogState extends State<DateTimeDialog> {
 
   @override
   Widget build(BuildContext context) => Padding(
-    padding: const EdgeInsets.all(16),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Text(
-          'Select date and time',
-          style: Theme.of(context).textTheme.headline6,
-        ),
-        const SizedBox(height: 16),
-        Row(
+        padding: const EdgeInsets.all(16),
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            RaisedButton(
-              child: Text(DateFormat('dd-MM-yyyy').format(selectedDate)),
-              onPressed: () async {
-                final date = await _selectDateTime(context,
-                    initialDate: selectedDate);
-                if (date == null) return;
-
-                setState(() {
-                  selectedDate = DateTime(
-                    date.year,
-                    date.month,
-                    date.day,
-                    selectedDate.hour,
-                    selectedDate.minute,
-                  );
-                });
-
-                widget.onSelectedDate(selectedDate);
-              },
+            Text(
+              'Select date and time',
+              style: TextStyle(
+                color: Color(0xff36244F),
+                fontWeight: FontWeight.w600,
+                fontSize: 18,
+              ),
             ),
-            const SizedBox(width: 8),
-            ElevatedButton(
-               style: ButtonStyle(),
-              child: Text(DateFormat('HH:mm').format(selectedDate)),
-              onPressed: () async {
-                final time =
-                await _selectTime(context, initialDate: selectedDate);
-                if (time == null) return;
-                setState(() {
-                  selectedDate = DateTime(
-                    selectedDate.year,
-                    selectedDate.month,
-                    selectedDate.day,
-                    time.hour,
-                    time.minute,
-                  );
-                });
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  color: Color(0xff36244F),
+                  child: Text(
+                    DateFormat('dd-MM-yyyy').format(selectedDate),
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () async {
+                    final date = await _selectDateTime(context,
+                        initialDate: selectedDate);
+                    if (date == null) return;
 
-                widget.onSelectedDate(selectedDate);
+                    setState(() {
+                      selectedDate = DateTime(
+                        date.year,
+                        date.month,
+                        date.day,
+                        selectedDate.hour,
+                        selectedDate.minute,
+                      );
+                    });
+
+                    widget.onSelectedDate(selectedDate);
+                  },
+                ),
+                const SizedBox(width: 8),
+                RaisedButton(
+                  color: Color(0xff36244F),
+                  child: Text(
+                    DateFormat('HH:mm').format(selectedDate),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () async {
+                    final time =
+                        await _selectTime(context, initialDate: selectedDate);
+                    if (time == null) return;
+                    setState(() {
+                      selectedDate = DateTime(
+                        selectedDate.year,
+                        selectedDate.month,
+                        selectedDate.day,
+                        time.hour,
+                        time.minute,
+                      );
+                    });
+
+                    widget.onSelectedDate(selectedDate);
+                  },
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            OutlineButton(
+              color: Color(0xff36244F),
+              borderSide: BorderSide(width: 3),
+              child: Text(
+                'OK',
+                style: TextStyle(
+                  color: Color(0xff36244F),
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  TextTimeInput(selectedDate.toIso8601String());
+                });
+                Navigator.of(context).pop();
               },
+              highlightColor: Colors.orange,
             ),
           ],
         ),
-        const SizedBox(height: 16),
-        OutlineButton(
-          child: Text('OK'),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          highlightColor: Colors.orange,
-        ),
-      ],
-    ),
-  );
+      );
 }
