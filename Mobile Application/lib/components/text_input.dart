c268da3b0216411ebeafe426076ae4327d028ee4
Ashna Nizam <68196153+AshNiz24@users.noreply.github.com>
2020-11-10 11:22:59 +0530

    Update text_input.dart

--- Mobile Application/lib/components/text_input.dart
+++ Mobile Application/lib/components/text_input.dart
@@ -18,94 +18,49 @@ class _TextTimeInputState extends State<TextTimeInput> {
   final DateFormat dateFormat = DateFormat('yyyy/MM/dd   HH:mm');
   @override
   Widget build(BuildContext context) {
-    if (widget.text == 'TO') {
-      return GestureDetector(
-        onTap: () async {
-          showDateTimeDialog(context, initialDate: selectedDate,
-              onSelectedDate: (selectedDate) {
-            setState(() {
-              this.selectedDate = selectedDate;
-              k = selectedDate;
-              print(this.selectedDate);
-            });
+    return GestureDetector(
+      onTap: () async {
+        showDateTimeDialog(context, initialDate: selectedDate,
+            onSelectedDate: (selectedDate) {
+          setState(() {
+            this.selectedDate = selectedDate;
+            k = selectedDate;
+            print(this.selectedDate);
           });
-        },
-        child: AbsorbPointer(
-          child: TextFormField(
-            autocorrect: true,
-            decoration: InputDecoration(
-              labelText:
-                  '${widget.text}\t\t\t\t\t\t\t\t\t\t\t\t\t ${dateFormat.format(selectedDate)}',
-              labelStyle: TextStyle(
-                  color: Color(0xff4A306D),
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500),
-              //suffix: Text('FROM $selectedDate'),
-              suffixIcon: Icon(
-                Icons.access_time,
-                color: Colors.deepPurple,
-                size: 30,
-              ),
-              hintText: widget.text,
-              hintStyle: TextStyle(color: Colors.grey, fontSize: 18),
-              filled: true,
-              fillColor: Colors.white70,
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                borderSide: BorderSide(color: Colors.deepPurple, width: 2),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                borderSide: BorderSide(color: Color(0xff4A306D)),
-              ),
+        });
+      },
+      child: AbsorbPointer(
+        child: TextFormField(
+          autocorrect: true,
+          decoration: InputDecoration(
+            labelText:(widget.text=='TO')?
+                '${widget.text}\t\t\t\t\t\t\t\t\t\t\t\t\t ${dateFormat.format(selectedDate)}':
+            '${widget.text}\t\t\t\t\t\t\t\t ${dateFormat.format(selectedDate)}',
+            labelStyle: TextStyle(
+                color: Color(0xff4A306D),
+                fontSize: 18,
+                fontWeight: FontWeight.w500),
+            //suffix: Text('FROM $selectedDate'),
+            suffixIcon: Icon(
+              Icons.access_time,
+              color: Colors.deepPurple,
+              size: 30,
+            ),
+            hintText: widget.text,
+            hintStyle: TextStyle(color: Colors.grey, fontSize: 18),
+            filled: true,
+            fillColor: Colors.white70,
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(12.0)),
+              borderSide: BorderSide(color: Colors.deepPurple, width: 2),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              borderSide: BorderSide(color: Color(0xff4A306D)),
             ),
           ),
         ),
-      );
-    } else {
-      return GestureDetector(
-        onTap: () async {
-          showDateTimeDialog(context, initialDate: selectedDate,
-              onSelectedDate: (selectedDate) {
-            setState(() {
-              this.selectedDate = selectedDate;
-              k = selectedDate;
-              print(this.selectedDate);
-            });
-          });
-        },
-        child: AbsorbPointer(
-          child: TextFormField(
-            autocorrect: true,
-            decoration: InputDecoration(
-              labelText:
-                  '${widget.text}\t\t\t\t\t\t\t\t ${dateFormat.format(selectedDate)}',
-              labelStyle: TextStyle(
-                  color: Color(0xff4A306D),
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500),
-              //suffix: Text('FROM $selectedDate'),
-              suffixIcon: Icon(
-                Icons.access_time,
-                color: Colors.deepPurple,
-                size: 30,
-              ),
-              hintText: widget.text,
-              hintStyle: TextStyle(color: Colors.grey, fontSize: 18),
-              filled: true,
-              fillColor: Colors.white70,
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                borderSide: BorderSide(color: Colors.deepPurple, width: 2),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                borderSide: BorderSide(color: Color(0xff4A306D)),
-              ),
-            ),
-          ),
-        ),
-      );
-    }
+      ),
+    );
   }
 }
